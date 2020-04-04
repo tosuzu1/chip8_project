815263940dd38a22dcce54e4dfb667c54da5e533
Felix Hong <felix.hong0321@gmail.com>
2020-04-03 14:31:18 -1000

    Most of the opcode should be done, changed stack to be located in memory instead of a seperate array, TODO: display , timers, keypress

--- chip8emulator.c
+++ chip8emulator.c
@@ -8,12 +8,12 @@
 #define DISPLAY_RESOLUTION_VERTICAL 32
 #define OPCODE_SIZE_INBYTES 2
 #define CHIP8_MEMORY_LIMIT 4096
-#define CHIP8_STACK_SIZE 12
+#define CHIP8_STACK_SIZE 16
 
 struct chip8processor { 
     uint8_t memory[CHIP8_MEMORY_LIMIT]; 
     uint8_t registers[16];
-    uint16_t stack[CHIP8_STACK_SIZE];
+    // uint16_t stack[CHIP8_STACK_SIZE];        //Stack is now inside of memory
     uint16_t stackSize;
     uint8_t delayTimer;
     uint8_t soundTimer;
@@ -61,13 +61,13 @@ int main(int argc, char *argv[]) {
     struct chip8processor p1;
     memset(p1.memory,0,sizeof(uint8_t)* CHIP8_MEMORY_LIMIT);
     memset(p1.registers,0,sizeof(uint8_t)*16);
-    memset(p1.stack,0,sizeof(uint16_t)*CHIP8_STACK_SIZE);
+    // memset(p1.stack,0,sizeof(uint16_t)*CHIP8_STACK_SIZE);
     p1.stackSize = 0;
     p1.delayTimer = 0;
     p1.soundTimer = 0;
     p1.userinput = 0;
     p1.programCounter = 0x200;
-    p1.stackPointer = 0;
+    p1.stackPointer = 0xea0;
     p1.addressRegister = 0;
 
     //Load rom into ram
@@ -106,8 +106,55 @@ int main(int argc, char *argv[]) {
     while(0) {
         if ((p1.memory[p1.programCounter] >> 4) == 0x0){
             //0x00 intructsions
+            if((p1.memory[p1.programCounter] & 0xf) == 0x0 && 
+            (p1.memory[p1.programCounter + 1] >> 4) == 0x0) {
+                //call clear screen
+            }
+            else if ((p1.memory[p1.programCounter] & 0xf) == 0 && 
+            (p1.memory[p1.programCounter + 1] >> 4) == 0xe) {
+                if(p1.stackSize == 0) {
+                    perror("ERROR: Stack underflowed into memory bounds\n");
+                }
+
+                uint16_t tempAddress = 0;
+                tempAddress = p1.memory[p1.stackPointer] & 0x0f;
+                tempAddress = tempAddress << 16;
+                tempAddress = tempAddress | p1.memory[p1.stackPointer + 1];
+
+                p1.stackPointer -= 2;
+                p1.stackSize -= 1;
+
+                p1.programCounter = tempAddress;
+                continue;
+            }
+            else {
+                // call subroutine RCA, this can go into below 0x200 region
+            
+                //Store program counter into stack
+                if(p1.stackSize > CHIP8_STACK_SIZE) {
+                    perror("ERROR: Stack overflowed into memory bounds\n");
+                }
+                p1.memory[p1.stackPointer] = ((p1.programCounter + 2) & 0xf00) >> 8;    //grab the programcounter's first 4 bits
+                p1.memory[p1.stackPointer + 1] = (p1.programCounter & 0xff);
+                p1.stackPointer += 2;
+                p1.stackSize += 1;
+
+                uint16_t tempAddress = 0;
+                tempAddress = p1.memory[p1.programCounter] & 0x0f;
+                tempAddress = tempAddress << 16;
+                tempAddress = tempAddress | p1.memory[p1.programCounter + 1]; 
+
+                if(tempAddress > 0xEA0) {
+                    perror("ERROR: Program ran out of memory bounds\n");
+                }
+
+                p1.programCounter = tempAddress;
+
+                continue;
+            }
         }
         else if ((p1.memory[p1.programCounter] >> 4) == 0x1) {
+            //jump command
             //Convert to a single variable
             uint16_t tempAddress = 0;
             tempAddress = p1.memory[p1.programCounter] & 0x0f;
@@ -123,6 +170,28 @@ int main(int argc, char *argv[]) {
         }
         else if ((p1.memory[p1.programCounter] >> 4) == 0x2) {
             // call subroutine
+            
+            //Store program counter into stack
+            if(p1.stackSize > CHIP8_STACK_SIZE) {
+                perror("ERROR: Stack overflowed into memory bounds\n");
+            }
+            p1.memory[p1.stackPointer] = ((p1.programCounter + 2) & 0xf00) >> 8;    //grab the programcounter's first 4 bits
+            p1.memory[p1.stackPointer + 1] = (p1.programCounter & 0xff);
+            p1.stackPointer += 2;
+            p1.stackSize += 1;
+
+            uint16_t tempAddress = 0;
+            tempAddress = p1.memory[p1.programCounter] & 0x0f;
+            tempAddress = tempAddress << 16;
+            tempAddress = tempAddress | p1.memory[p1.programCounter + 1]; 
+
+            if(tempAddress < 0x200 || tempAddress > 0xEA0) {
+                perror("ERROR: Program ran out of memory bounds\n");
+            }
+
+            p1.programCounter = tempAddress;
+
+            continue;
         }
         else if ((p1.memory[p1.programCounter] >> 4) == 0x3) {
             // Skip if Vx == NN
@@ -152,7 +221,42 @@ int main(int argc, char *argv[]) {
             p1.registers[p1.memory[p1.programCounter] & 0xf] += p1.memory[p1.programCounter + 1];
         }
         else if ((p1.memory[p1.programCounter] >> 4) == 0x8) {
-            
+            // Commmand Vx = Vy
+            if((p1.memory[p1.programCounter + 1] & 0x0f) == 0x0) {
+                p1.registers[p1.memory[p1.programCounter] & 0x0f] = p1.registers[p1.memory[p1.programCounter + 1] & 0xf0];
+            }
+            // Command Vx = Vx | Vy
+            else if ((p1.memory[p1.programCounter + 1] & 0x0f) == 0x1) {
+                p1.registers[p1.memory[p1.programCounter] & 0x0f] = p1.registers[p1.memory[p1.programCounter + 1] & 0xf0] | p1.registers[p1.memory[p1.programCounter] & 0x0f];
+            }
+            // Commmand 	Vx=Vx&Vy
+            else if ((p1.memory[p1.programCounter + 1] & 0x0f) == 0x2) {
+                p1.registers[p1.memory[p1.programCounter] & 0x0f] = p1.registers[p1.memory[p1.programCounter + 1] & 0xf0] & p1.registers[p1.memory[p1.programCounter] & 0x0f];
+            }
+            // Commmand 	Vx=Vx^Vy
+            else if ((p1.memory[p1.programCounter + 1] & 0x0f) == 0x3) {
+                p1.registers[p1.memory[p1.programCounter] & 0x0f] = p1.registers[p1.memory[p1.programCounter + 1] & 0xf0] ^ p1.registers[p1.memory[p1.programCounter] & 0x0f];
+            }
+            // Command Vx += Vy
+            else if ((p1.memory[p1.programCounter + 1] & 0x0f) == 0x4) {
+                p1.registers[p1.memory[p1.programCounter] & 0x0f] += p1.registers[p1.memory[p1.programCounter + 1] & 0xf0];
+            }
+            // Command Vx -= Vy
+            else if ((p1.memory[p1.programCounter + 1] & 0x0f) == 0x5) {
+                p1.registers[p1.memory[p1.programCounter] & 0x0f] -= p1.registers[p1.memory[p1.programCounter + 1] & 0xf0];
+            }
+            // Command Vx >>= 1
+            else if ((p1.memory[p1.programCounter + 1] & 0x0f) == 0x6) {
+                p1.registers[p1.memory[p1.programCounter] & 0x0f] = p1.registers[p1.memory[p1.programCounter] & 0x0f] >> 1;
+            }
+            // Command Vx = Vy - Vx
+            else if ((p1.memory[p1.programCounter + 1] & 0x0f) == 0x7) {
+                p1.registers[p1.memory[p1.programCounter] & 0x0f] = p1.registers[p1.memory[p1.programCounter + 1] & 0xf0] - p1.registers[p1.memory[p1.programCounter] & 0x0f];
+            }
+            // Command Vx <<= 1
+            else if ((p1.memory[p1.programCounter + 1] & 0x0f) == 0xe) {
+                p1.registers[p1.memory[p1.programCounter] & 0x0f] = p1.registers[p1.memory[p1.programCounter] & 0x0f] << 1;
+            }
         }
         else if ((p1.memory[p1.programCounter] >> 4) == 0x9) {
             // Skip if Vx != Vy
@@ -212,7 +316,16 @@ int main(int argc, char *argv[]) {
                 //sound timer
             }
             else if(p1.memory[p1.programCounter + 1] == 0x1e) {
+                p1.addressRegister += p1.registers[p1.memory[p1.programCounter] & 0x0f];
 
+                uint16_t tempAddress = p1.addressRegister + p1.registers[p1.memory[p1.programCounter] & 0x0f];
+
+                if(tempAddress > 0xFFF) {
+                    p1.registers[15] = 1;
+                }
+                else {
+                    p1.registers[15] = 0;
+                }
             
             }
             else if(p1.memory[p1.programCounter + 1] == 0x29) {
@@ -220,12 +333,22 @@ int main(int argc, char *argv[]) {
             }
             else if(p1.memory[p1.programCounter + 1] == 0x33) {
                 // BCD
+                p1.memory[p1.addressRegister] = p1.registers[p1.memory[p1.programCounter] & 0x0f] / 100;
+                p1.memory[p1.addressRegister + 1] = (p1.registers[p1.memory[p1.programCounter] & 0x0f] % 100) / 10 ;
+                p1.memory[p1.addressRegister + 2] = (p1.registers[p1.memory[p1.programCounter] & 0x0f] % 10);
             }
             else if(p1.memory[p1.programCounter + 1] == 0x55) {
                 // REGDUMP
+                for (uint8_t i = 0; i <= (p1.memory[p1.programCounter] & 0x0f); i += 1) {
+                    p1.memory[p1.addressRegister + i]  = p1.registers[i];
+                    
+                }
             }
             else if(p1.memory[p1.programCounter + 1] == 0x65) {
                 // Regload
+                for (uint8_t i = 0; i <= (p1.memory[p1.programCounter] & 0x0f); i += 1) {
+                    p1.registers[i] = p1.memory[p1.addressRegister + i];
+                }
             }
             else {
                 printf("DEBUG: opcode at 0xfxxx instruction\n");
@@ -237,6 +360,7 @@ int main(int argc, char *argv[]) {
 
         // Increment programer
         p1.programCounter += 2;
+        
     }
 
     return 1;
